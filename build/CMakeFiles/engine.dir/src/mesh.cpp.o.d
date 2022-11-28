44f2e5600b13c23d10f8b2b0bc9ef09fd35d834e
thanksforcomin <awesome.bahin@yandex.ru>
2022-11-28 15:46:30 +0300

    cleaning

--- build/CMakeFiles/engine.dir/src/mesh.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/engine.dir/src/mesh.cpp.o: \
-  /home/eriksson/code/minivan/src/mesh.cpp
--- build/CMakeFiles/engine.dir/src/model.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/engine.dir/src/model.cpp.o: \
-  /home/eriksson/code/minivan/src/model.cpp \
-  /home/eriksson/code/minivan/src/model.h
